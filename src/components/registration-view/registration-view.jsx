31774944b6c899b9ce5bc014906b84d4d03d5072
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2022-01-25 14:41:00 -0700

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -54,6 +54,9 @@ export function RegistrationView() {
                     alt="AvengersFlix logo"
                 />vengersFlix
             </h1>
+            <p className="d-flex text-center mb-5 w-50" style={{ fontWeight: 600, fontSize: "17px" }}>
+                An application that consists of the most popular movies of the Avengers heroes. Created for fans to be able view Avengers movies and their details in one location. Join us!
+            </p>
             <Form className="registration-view" onSubmit={handleSubmit} style={{ textAlign: "center" }}>
                 <h1 style={{ fontFamily: 'Montserrat', fontWeight: 700 }}>Register</h1>
                 <FloatingLabel controlId="formUsername" label="Username*" className="mb-3 mt-4">
